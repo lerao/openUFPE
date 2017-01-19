55a9d7c243e9db53e557e64d4b5cb33dfaa7ad8a
filipeepifaniodeoliveira <feo@cin.ufpe.br>
2017-01-19 01:02:40 -0300

    fechamento de versÃo

--- controllers/consultas.js
+++ controllers/consultas.js
@@ -1,3 +1,128 @@
-app.controller('consultasCtrl', function($scope) {
-  $scope.message = 'You can see more about ngRoute in the oficial website.';
+app.controller('consultasCtrl', function($scope, $http) {
+
+	$scope.datasets = [
+		{
+			"nome": "Pessoa",
+			"datasetName":"pessoa"
+		},
+		{
+			"nome": "Alunos",
+			"datasetName":"aluno"
+		},
+		{
+			"nome": "Docentes",
+			"datasetName":"docentes"
+		},
+		{
+			"nome": "Curso",
+			"datasetName": "curso"
+		},
+		{
+			"nome": "Turma",
+			"datasetName":"turma"
+		},
+		{
+			"nome": "Orgão",
+			"datasetName":"orgao"
+		}
+	];
+   
+	$scope.formatos = [
+		{
+		
+			"nome": "JSON",
+			"format":"json"
+		},
+		{
+			"nome": "XML",
+			"format":"xml"
+		},
+		//{
+		//	"nome": "CSV",
+		//	"format":"csv"
+		//},
+		{
+			"nome": "RDF",
+			"format": "rdf"
+		}
+	];
+
+	$scope.download = {
+		show: false
+	};
+
+	$scope.buscar =  function (data) {
+    console.log(data);
+
+		if(!data || !data.datasetName) {
+			alert("Selecione o dataset");
+			return;
+		}
+
+		if (!data || !data.format) {
+			alert("Selecione o formato");
+			return;
+		}
+
+		if(data.where == "") data.where = {};
+
+		$http({
+	      method: 'POST',
+	      url: 'http://localhost:8080/find/execute_query',
+	      data: data,
+	      headers: {
+	        'Content-Type' : 'application/json'
+	      }
+	    }).then(function successCallback(res) {
+	    	console.log(res);
+	    	if(res.data){ 
+
+	    		$scope.download.data = res.data;
+	    		$scope.download.format = data.format;
+	    		$scope.download.show = true;
+	  		 // console.log(res.data);
+	    	
+	    	} else {
+
+	    		alert("Não houve retorno");
+	    	}
+
+	    }, function errorCallback(res) {
+	     	console.log('deu erro');
+	     	alert("Não houve retorno para a consulta");
+	    });
+    
+    };	
+
+
+
+    $scope.fazerDownload = function () {
+
+    	var format = $scope.download.format.toLowerCase();
+    	var data = $scope.download.data;
+    	var contentType = "text/csv;charset=utf-8";
+
+
+    	if(format == "json") {
+    		contentType = "application/json;charset=utf-8";
+    		data = JSON.stringify(data);
+    	} else if(format == "xml") {
+    		contentType = "application/xml;charset=utf-8";
+    	} else {
+    		contentType = "application/rdf+xm;charset=utf-8";
+    	}
+
+    	var blob = new Blob([data], { type: contentType });
+    	var fileName = "file." + format;
+
+    	var link = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+       	link.href = (window.URL || window.webkitURL).createObjectURL(blob);
+       	link.download = fileName;
+        
+	    var event = document.createEvent("MouseEvents");
+	    event.initEvent("click", true, false);
+	    link.dispatchEvent(event);
+
+    }
+		
 });
--- controllers/indicadores.js
+++ controllers/indicadores.js
@@ -1,46 +1,113 @@
-app.controller('indicadoresCtrl', chartController);
+app.controller('indicadoresCtrl',['$scope', '$http', function($scope, $http) {
 
-chartController.$inject = ['$scope', '$http'];
+  $scope.pegarDados = function () {
+    pegarDadosSI();
+    pegarDadosCC();
+  }
 
-function chartController ($scope, $http) {
 
-    //grafico
-    //$scope.labels = ['2008', '2007', '2008', '2009', '2010', '2011', '2012'];
-    //$scope.series = ['Numero de repovacoes no ultimo semestre', 'numero de reprovacoes da disciplina'];
-    //$scope.data = [
-    //[65, 59, 80, 81, 56, 55, 40],
-    //[200, 48, 40, 19, 86, 27, 90]
-    //];
-  
-  function pegarDados () {
+
+//alunos do curso de SI que abandonaram o curso de 2012 a 2016
+function pegarDadosSI () {
     console.log('chamou o pegar dados');
 
+//alunos admitidos em 2016 que desistiram do curso
+
     $http({
-      method: 'GET',
-      url: 'http://localhost:8080/find/orgao/format/json',
-      headers: {
-        'Content-Type' : 'application/json'
-      }
-    }).then(function successCallback(res) {
-      $scope.indicadores = res.data;
-      console.log(res.data);
+          url: 'http://localhost:8080/find/execute_query',
+          method: 'POST',
+          data: {
 
-      
-      var dados   = res.data;
-      var cursos  = [];
-      var mediaCursos = 0;
-
-      //
-      for (var i = 0; i < dados.length; i++) {
-      //
-        if (dados.tipo == "curso") {
-          cursos.push(dados.tipo);
-          mediaCursos ++;
+        "datasetName":"aluno_SI", 
+        "format":"json",
+        "where":
+        { 
+          "codg_progm_fomc" : "311",
+          "codg_tipo_sitc_acadmc" :"17"
         }
-      //
-       }
-      //
-      // $scope.cursos = cursos;
+         
+     
+        },
+          headers: {
+            'Content-Type': 'application/json'            
+          }
+          
+        }).then(function successCallback(res) {
+          $scope.indicadores = res.data;
+          //console.log(res.data);
+
+           var desistiu2011 = 0;
+           var desistiu2012 = 0;
+           var desistiu2013 = 0;
+           var desistiu2014 = 0;
+           var desistiu2015 = 0;
+           var desistiu2016 = 0;
+           
+           var dados   = res.data;
+           console.log(dados);
+          // var cursos  = [];
+          // var qtdAlunos = 35;
+
+
+          //    if 17
+          //     if ano_adms
+          //     else if ano_adms
+
+
+
+
+           for (var i = 0; i< dados.length; i++) {
+           
+            if (dados[i].ano_adms == "2011" && dados[i].codg_tipo_sitc_acadmc == "17"){
+            desistiu2011 = desistiu2011 + 1;
+            console.log(desistiu2011 + "desistentes em 2011");
+
+          }
+
+            else if (dados[i].ano_adms == "2012" && dados[i].codg_tipo_sitc_acadmc == "17"){
+            desistiu2012 = desistiu2012 + 1;
+            console.log(desistiu2012 + "desistentes em 2012");
+
+          }
+
+            else if (dados[i].ano_adms == "2013" && dados[i].codg_tipo_sitc_acadmc == "17"){
+            desistiu2013 = desistiu2013 + 1;
+            console.log(desistiu2013 + "desistentes em 2013");
+
+          }
+
+            else if (dados[i].ano_adms == "2014" && dados[i].codg_tipo_sitc_acadmc == "17"){
+            desistiu2014 = desistiu2014 + 1;
+            console.log(desistiu2014 + "desistentes em 2014");
+
+          } else if ( dados[i].ano_adms == "2015" && dados[i].codg_tipo_sitc_acadmc == "17"){
+
+            desistiu2015 = desistiu2015 +1;
+            console.log(desistiu2015 + "desistentes em 2015");
+
+          } else if ( dados[i].ano_adms == "2016" && dados[i].codg_tipo_sitc_acadmc == "17"){
+
+            desistiu2016 = desistiu2016 +1;
+            console.log(desistiu2016 + "desistentes em 2016");
+
+          }
+
+         } 
+       
+
+          //grafico
+          $scope.bar_si = {
+
+            labels : ['2011','2012', '2013', '2014', '2015', '2016'],
+            series : ['Numero de alunos que abandonaram o curso'],
+            data : [
+                   [desistiu2011, desistiu2012, desistiu2013, desistiu2014, desistiu2015, desistiu2016]
+                ]
+          };
+
+         // cursos.push(dados.tipo);
+         // mediaCursos ++;
+         // $scope.cursos = cursos;
 
     }, function errorCallback(res) {
       console.log('deu erro');
@@ -48,10 +115,110 @@ function chartController ($scope, $http) {
 
   }
 
-  $scope.pegarDados = pegarDados;
 
-}
+  
+//alunos do curso de CC que abandonaram o curso de 2012 a 2016
+  function pegarDadosCC () {
+    console.log('chamou o pegar dados');
+
+//alunos admitidos em 2016 que desistiram do curso
+
+    $http({
+          url: 'http://localhost:8080/find/execute_query',
+          method: 'POST',
+          data: {
+
+        "datasetName":"aluno_CC", 
+        "format":"json",
+        "where":
+        { 
+          "codg_progm_fomc" : "45",
+          "codg_tipo_sitc_acadmc" :"17"
+        }
+         
+     
+        },
+          headers: {
+            'Content-Type': 'application/json'            
+          }
+          
+        }).then(function successCallback(cc) {
+          $scope.indicadores = cc.data;
+          //console.log(res.data);
+
+           var desistiuCC2011 = 0;
+           var desistiuCC2012 = 0;
+           var desistiuCC2013 = 0;
+           var desistiuCC2014 = 0;
+           var desistiuCC2015 = 0;
+           var desistiuCC2016 = 0;
+           
+           
+           var dadosCC   = cc.data;
+           console.log(dadosCC);
+          // var cursos  = [];
+          // var qtdAlunos = 35;
+
+           for (var i = 0; i< dadosCC.length; i++) {
+           
+          if (dadosCC[i].ano_adms == "2011" && dadosCC[i].codg_tipo_sitc_acadmc == "17"){
+            desistiuCC2011 = desistiuCC2011 + 1;
+            console.log(desistiuCC2011 + "desistentes CC em 2011");
+
+          }
+
+          else if (dadosCC[i].ano_adms == "2012" && dadosCC[i].codg_tipo_sitc_acadmc == "17"){
+            desistiuCC2012 = desistiuCC2012 + 1;
+            console.log(desistiuCC2012 + "desistentes CC em 2012");
+
+          }
+
+            else if (dadosCC[i].ano_adms == "2013" && dadosCC[i].codg_tipo_sitc_acadmc == "17"){
+            desistiuCC2013 = desistiuCC2013 + 1;
+            console.log(desistiuCC2013 + "desistentes CC em 2013");
+
+          }
+
+            else if (dadosCC[i].ano_adms == "2014" && dadosCC[i].codg_tipo_sitc_acadmc == "17"){
+            desistiuCC2014 = desistiuCC2014 + 1;
+            console.log(desistiuCC2014 + "desistentes CC em 2014");
+
+          } else if ( dadosCC[i].ano_adms == "2015" && dadosCC[i].codg_tipo_sitc_acadmc == "17"){
+
+            desistiuCC2015 = desistiuCC2015 +1;
+            console.log(desistiuCC2015 + "desistentes CC em 2015");
+
+          } else if ( dadosCC[i].ano_adms == "2016" && dadosCC[i].codg_tipo_sitc_acadmc == "17"){
+
+            desistiuCC2016 = desistiuCC2016 +1;
+            console.log(desistiuCC2016 + "desistentes CC em 2016");
+
+          }
+
+         } 
+
+          $scope.bar_cc = {
+
+            labels : ['2011','2012', '2013', '2014', '2015', '2016'],
+            series : ['Numero de alunos que abandonaram o curso'],
+          
+            data : [
+                   [desistiuCC2011, desistiuCC2012, desistiuCC2013, desistiuCC2014, desistiuCC2015, desistiuCC2016]
+                  ]
+          };
+
+         // cursos.push(dados.tipo);
+         // mediaCursos ++;
+         // $scope.cursos = cursos;
+
+    }, function errorCallback(res) {
+      console.log('deu erro');
+    });
+
+  }
+
+  $scope.pegarDadosSI = pegarDadosSI;
+  $scope.pegarDadosCC = pegarDadosCC;
 
 
-
- 
+ }]);
